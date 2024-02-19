570fd94482a2ac946743d073fd3156e3e16ae36b
Raj Machine Solution <154319712+RajMachineSolution@users.noreply.github.com>
2024-02-19 18:32:41 +0530

    Add files via upload

---
+++ 14feb labelForm/14feb labelForm/Form1.vb
@@ -0,0 +1,11 @@
+﻿Public Class Form1
+
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+
+        Call UserControl11.ShowDataInLabel()
+
+    End Sub
+
+
+End Class
